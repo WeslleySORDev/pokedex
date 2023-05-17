6a1c0d847ef892cea74a1bef60720b3350d36298
WeslleySORDev <weslleysordev@gmail.com>
2023-05-17 08:41:11 -0300

    add elevation classes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,10 +55,10 @@ export default function Home() {
   }, [inView]);
   return (
     <main>
-      <p className="headline">headline</p>
-      <p className="subtitle-1">subtitle-1</p>
-      <p className="subtitle-2">subtitle-2</p>
-      <p className="subtitle-3">subtitle-3</p>
+      <p className="headline dropshadow-2dp">headline</p>
+      <p className="subtitle-1 dropshadow-6dp mt-4">subtitle-1</p>
+      <p className="subtitle-2 inner-shadow mt-4">subtitle-2</p>
+      <p className="subtitle-3 mt-4">subtitle-3</p>
       {/* {status === "loading" ? (
         <p>Loading...</p>
       ) : (
