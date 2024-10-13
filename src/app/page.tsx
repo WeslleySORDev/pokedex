0c3f8d6a40bc2404a91f970420878e3315de73ac
WeslleySORDev <weslleysordev@gmail.com>
2024-10-13 11:47:49 -0300

    feat: configuração tanstack query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import allPokemonNameAndUrlFromPokeApi from "./utils/all-pokemon-name-and-url-from-poke-api.json";
+
+const MAX_ITEMS_ON_PAGE = 20;
+
 export default function Home() {
+  const split_array_into_chunks = (
+    array: string[],
+    size: number
+  ): string[][] => {
+    const result: string[][] = [];
+    for (let i = 0; i < array.length; i += size) {
+      const chunk = array.slice(i, i + size);
+      result.push(chunk);
+    }
+    return result;
+  };
+  const all_pokemon_url_divided_by_pages = split_array_into_chunks(
+    allPokemonNameAndUrlFromPokeApi.pokemons.map((pokemon) => pokemon.url),
+    MAX_ITEMS_ON_PAGE
+  );
   return (
     <main>
-      <h1>Home</h1>
+      {all_pokemon_url_divided_by_pages[0].map(pokemon => (
+        <p>{pokemon}</p>
+      ))}
     </main>
   );
 }
